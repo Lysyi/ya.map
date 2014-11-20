7a944b455fee2e6c5dc3e99fbc1388ea568965ce
Sasha <oleksandr.lysyi@gmail.com>
2014-11-20 05:52:41 +0200

    added icons and markers

--- js/groups.js
+++ js/groups.js
@@ -5,31 +5,61 @@ var groups = [
         items: [
             {
                 name: "Достопримечательности",
-                img: "img/img1.png",
+                img: "img/mark-1.png",
                 style: "default#image",
                 items: [
                     {
-                        name: "Фонтан",
-                        center: [48.47533341, 135.06479422]
+                        name: "Фонтан-11",
+                        center: [48.473508,135.066672]
                     },
                     {
-                        name: "Набережная",
-                        center: [48.47523391, 135.06809472]
+                        name: "Фонтан-12",
+                        center: [48.472025,135.054270]
                     }
                 ]
             },
             {
                 name: "Музеи",
-                img: "img/img2.png",
+                img: "img/mark-1.png",
                 style: "default#image",
                 items: [
                     {
-                        name: "Музей города",
-                        center: [48.47230342, 135.06979412]
+                        name: "Музей-11",
+                        center: [48.473279,135.050979]
                     },
                     {
-                        name: "Арт музей",
-                        center: [48.47503345, 135.06779402]
+                        name: "Музей-12",
+                        center: [48.476418,135.074926]
+                    }
+                ]
+            },
+            {
+                name: "Театры",
+                img: "img/mark-1.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Театр-11",
+                        center: [48.475733,135.051919]
+                    },
+                    {
+                        name: "Театр-12",
+                        center: [48.479529,135.070710]
+                    }
+                ]
+            },
+            {
+                name: "Библиотеки",
+                img: "img/mark-1.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Библиотека-11",
+                        center: [48.475227,135.077019]
+                    },
+                    {
+                        name: "Библиотека-12",
+                        center: [48.480021,135.057879]
                     }
                 ]
             }
@@ -40,35 +70,334 @@ var groups = [
         ico: "ico2",
         items: [
             {
-                name: "Кинотеатр",
-                img: "img/img3.png",
+                name: "Кинотеатры",
+                img: "img/mark-2.png",
                 style: "default#image",
                 items: [
                     {
-                        name: "Центральный",
-                        center: [48.47552391, 135.06474472]
+                        name: "Кино-1",
+                        center: [48.476768,135.065491]
                     },
                     {
-                        name: "Большой",
+                        name: "Кино-2",
                         center: [48.47133341, 135.06239402]
                     }
                 ]
             },
             {
                 name: "Караоке",
-                img: "img/img4.png",
+                img: "img/mark-2.png",
                 style: "default#image",
                 items: [
                     {
-                        name: "Клуб",
+                        name: "Клуб-1",
                         center: [48.47523092, 135.06871472]
                     },
                     {
-                        name: "Компания",
+                        name: "Клуб-2",
                         center: [48.47310301, 135.06872472]
+                    }
+                ]
+            },
+            {
+                name: "Парки",
+                img: "img/mark-2.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Парк-1",
+                        center: [48.477082,135.048068]
+                    },
+                    {
+                        name: "Парк-2",
+                        center: [48.473030,135.053132]
+                    }
+                ]
+            },
+            {
+                name: "Развлекательный комплекс",
+                img: "img/mark-2.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "РК-1",
+                        center: [48.475085,135.058968]
+                    },
+                    {
+                        name: "РК-2",
+                        center: [48.479020,135.065105]
+                    }
+                ]
+            },
+            {
+                name: "Ночной клуб",
+                img: "img/mark-2.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "НК-1",
+                        center: [48.472440,135.049205]
+                    }
+                ]
+            },
+            {
+                name: "Стриптыз бар",
+                img: "img/mark-2.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "НК-1",
+                        center: [48.477390,135.045300]
                     }
                 ]
             }
         ]
-    }
+    },
+    {
+        name: "Рестораны",
+        ico: "ico3",
+        items: [
+            {
+                name: "Рестораны",
+                img: "img/mark-3.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Ресторан-11",
+                        center: [48.474019,135.059424]
+                    }
+                ]
+            },
+            {
+                name: "Кафе",
+                img: "img/mark-3.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Кафе-11",
+                        center: [48.475118,135.058988]
+                    }
+                ]
+            },
+            {
+                name: "Бары",
+                img: "img/mark-3.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Бар-11",
+                        center: [48.472957,135.065056]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: "Красота и здоровье",
+        ico: "ico4",
+        items: [
+            {
+                name: "Медицинский центр",
+                img: "img/mark-4.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "МЦ-11",
+                        center: [48.472071,135.059928]
+                    }
+                ]
+            },
+            {
+                name: "Спа-салон",
+                img: "img/mark-4.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Салон-11",
+                        center: [48.475197,135.065443]
+                    }
+                ]
+            },
+            {
+                name: "Сауна",
+                img: "img/mark-4.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Бар-11",
+                        center: [48.470801,135.060293]
+                    }
+                ]
+            },
+            {
+                name: "Аптека",
+                img: "img/mark-4.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Аптека-11",
+                        center: [48.474297,135.067331]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: "Дети",
+        ico: "ico5",
+        items: [
+            {
+                name: "Детские площадки",
+                img: "img/mark-5.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Площадка-11",
+                        center: [48.471969,135.072330]
+                    },
+                    {
+                        name: "Площадка-12",
+                        center: [48.470999,135.054961]
+                    }
+                ]
+            },
+            {
+                name: "Развлекательные центры",
+                img: "img/mark-5.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "РЦ-11",
+                        center: [48.472482,135.058182]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: "Спорт",
+        ico: "ico6",
+        items: [
+            {
+                name: "Фитнес-клуб",
+                img: "img/mark-6.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "ФК-11",
+                        center: [48.479983,135.044425]
+                    }
+                ]
+            },
+            {
+                name: "Спорт-бар",
+                img: "img/mark-6.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Спорт-бар-11",
+                        center: [48.478895,135.050862]
+                    }
+                ]
+            },
+            {
+                name: "Стадион",
+                img: "img/mark-6.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Стадион-11",
+                        center: [48.478390,135.046528]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: "Магазины",
+        ico: "ico7",
+        items: [
+            {
+                name: "Одежда",
+                img: "img/mark-7.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Магазын-11",
+                        center: [48.475594,135.059317]
+                    },
+                    {
+                        name: "Магазин-11",
+                        center: [48.469773,135.057493]
+                    }
+                ]
+            },
+            {
+                name: "Для детей",
+                img: "img/mark-7.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "ДетМаг-11",
+                        center: [48.477663,135.066333]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: "Администрация",
+        ico: "ico8",
+        items: [
+            {
+                name: "Администрация города",
+                img: "img/mark-8.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Администрация-11",
+                        center: [48.473326,135.075260]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: "Глав.почтамп",
+        ico: "ico9",
+        items: [
+            {
+                name: "Глав.почтамп",
+                img: "img/mark-9.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Почта-11",
+                        center: [48.472070,135.070293]
+                    },
+                    {
+                        name: "Почта-12",
+                        center: [48.474809,135.077653]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: "Авиакассы",
+        ico: "ico10",
+        items: [
+            {
+                name: "Авиакассы",
+                img: "img/mark-10.png",
+                style: "default#image",
+                items: [
+                    {
+                        name: "Касса-11",
+                        center: [48.473369,135.068861]
+                    }
+                ]
+            }
+        ]
+    },
 ];
--- js/object_list.js
+++ js/object_list.js
@@ -2,25 +2,22 @@ ymaps.ready(init);
 
 function init() {
 
-    // Создание экземпляра карты.
     var myMap = new ymaps.Map('map', {
-            center: [48.47533343, 135.06479423],
-            zoom: 16,
+            center: [48.47458821, 135.06077406],
+            zoom: 15.5,
             controls: []
         });
     
-    var myPlacemark = new ymaps.Placemark([48.477, 135.06], {
-            hintContent: 'Собственный значок метки'
+    var myPlacemark = new ymaps.Placemark([48.476443, 135.058188], {
+            hintContent: 'Хабаровск Сити'
         }, {
             iconLayout: 'default#image',
-            iconImageHref: 'img/img0.png',
+            iconImageHref: 'img/main-mark.png',
             iconImageSize: [208, 96],
-            iconImageOffset: [-15, -96]
+            iconImageOffset: [-104, -96]
         });
 
     var mapMenu = $('<ul class="map-menu"></ul>'); 
-    
-    myMap.geoObjects.add(myPlacemark);
         
     for (var i = 0, l = groups.length; i < l; i++) {
         createSuperGroup(groups[i]);
@@ -51,37 +48,29 @@ function init() {
     }
 
     function createMenuGroup (group, menuGroup) {
-        // Пункт меню.
         var menuItem = $('<li><a href="#">' + group.name + '</a></li>'),
-            // Коллекция для геообъектов группы.
             collection = new ymaps.GeoObjectCollection(null, {
             iconLayout: group.style,
             iconImageHref: group.img,
             iconImageSize: [32, 37],
-            iconImageOffset: [-5, -37]
+            iconImageOffset: [-16, -37]
             }),
-        // Контейнер для подменю.
             secondSubmenu = $('<ul class="map-second-submenu" style="display:none;"></ul>');
 
         // Добавляем коллекцию на карту.
         //myMap.geoObjects.add(collection);
 
-        // Добавляем подменю.
         menuItem
             .append(secondSubmenu)
-            // Добавляем пункт в меню.
             .appendTo(menuGroup)
-            // По клику удаляем/добавляем коллекцию на карту и скрываем/отображаем подменю.
             .find('a')
             .toggle(
             function () {
                 myMap.geoObjects.add(collection);
-                // secondSubmenu.show();
                 menuItem.addClass('active');
             },
             function () {
                 myMap.geoObjects.remove(collection);
-                // secondSubmenu.hide();
                 menuItem.removeClass('active');
             });
         for (var j = 0, m = group.items.length; j < m; j++) {
@@ -90,17 +79,12 @@ function init() {
     }
 
     function createSubMenu (item, collection, submenu) {
-        // Пункт подменю.
         var submenuItem = $('<li><a href="#">' + item.name + '</a></li>'),
-        // Создаем метку.
             placemark = new ymaps.Placemark(item.center, { hintContent: item.name });
 
-        // Добавляем метку в коллекцию.
         collection.add(placemark);
-        // Добавляем пункт в подменю.
         submenuItem
             .appendTo(submenu)
-            // При клике по пункту подменю открываем/закрываем баллун у метки.
             .find('a')
             .toggle(function () {
                 placemark.balloon.open();
@@ -109,8 +93,8 @@ function init() {
             });
     }
 
-    // Добавляем меню в тэг BODY.
     mapMenu.appendTo($('.left-side'));
-    // Выставляем масштаб карты чтобы были видны все группы.
-    // myMap.setBounds(myMap.geoObjects.getBounds());
+
+    myMap.setBounds(myMap.geoObjects.getBounds());
+    myMap.geoObjects.add(myPlacemark);
 }
